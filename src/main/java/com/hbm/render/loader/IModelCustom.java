80569aa8158adcb4077ebf473820bd7b80c1ff94
HIRABRINUS <oniterraria@gmail.com>
2026-05-03 19:03:49 +0300

    Update IModelCustom.java

--- src/main/java/com/hbm/render/loader/IModelCustom.java
+++ src/main/java/com/hbm/render/loader/IModelCustom.java
@@ -1,1 +1,14 @@
+package com.hbm.render.loader;
 
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+@SideOnly(Side.SERVER)
+public interface IModelCustom
+{
+	String getType();
+    void renderAll();
+    void renderOnly(String... groupNames);
+    void renderPart(String partName);
+    void renderAllExcept(String... excludedGroupNames);
+}
